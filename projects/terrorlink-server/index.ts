aeb834f77363bf208d662095c20b43be71225cce
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-02-21 09:56:28 +0100

    add cssharp plugin

--- projects/terrorlink-server/index.ts
+++ projects/terrorlink-server/index.ts
@@ -1,7 +1,26 @@
 import { env } from "bun";
 import Elysia from "elysia";
+import { existsSync, mkdirSync, rmdirSync, writeFileSync } from "node:fs";
 
 console.log("Hello via Bun!");
 const port = Number.parseInt(env.WEBSERVER_PORT as string);
 
-new Elysia().listen(port, () => console.log(`Server is running on ${port}`));
+const messagesDirectory = "./messages";
+
+if (existsSync(messagesDirectory)) {
+	rmdirSync(messagesDirectory, { recursive: true });
+}
+
+mkdirSync(messagesDirectory);
+let messageCount = -1;
+
+new Elysia()
+	.post("/api/gsi", (context) => {
+		console.log(context.body);
+		messageCount++;
+		writeFileSync(
+			`${messagesDirectory}/message-${messageCount}.json`,
+			JSON.stringify(context.body, null, 2),
+		);
+	})
+	.listen(port, () => console.log(`Server is running on ${port}`));
