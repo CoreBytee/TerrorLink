3aa3a7d42f7fbf2aae670412527ccff1c6145ed3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-02-21 13:40:38 +0100

    send tick data

--- projects/terrorlink-plugin/TerrorLink.cs
+++ projects/terrorlink-plugin/TerrorLink.cs
@@ -1,4 +1,10 @@
 ﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using CounterStrikeSharp.API;
 
 namespace TerrorLink;
 public class TerrorLink : BasePlugin
@@ -10,5 +16,88 @@ public class TerrorLink : BasePlugin
     public override void Load(bool hotReload)
     {
         Console.WriteLine("Hello World!");
+
+        RegisterListener<Listeners.OnTick>(() =>
+        {
+            var gamerules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
+
+            this.SendEvent(new
+            {
+                type = "tick",
+                time = DateTime.Now,
+                tick = Server.TickCount,
+                data = new
+                {
+                    map = Server.MapName,
+                    round = gamerules.TotalRoundsPlayed,
+                    round_phase = gamerules.GamePhase,
+                    players = Utilities.GetPlayers().Select(player => new
+                    {
+                        sid = player.SteamID,
+                        name = player.PlayerName,
+                        health = player.PawnHealth,
+                        armor = player.PawnArmor,
+                        is_bot = player.IsBot,
+                        is_alive = player.PawnIsAlive,
+                        is_walking = player.PlayerPawn?.Value?.IsWalking,
+                        team = player.Team,
+                        velocity = new
+                        {
+                            x = player.PlayerPawn?.Value?.Velocity.X ?? 0,
+                            y = player.PlayerPawn?.Value?.Velocity.Y ?? 0,
+                            z = player.PlayerPawn?.Value?.Velocity.Z ?? 0
+                        },
+                        position = new
+                        {
+                            x = player.PlayerPawn?.Value?.AbsOrigin!.X ?? 0,
+                            y = player.PlayerPawn?.Value?.AbsOrigin!.Y ?? 0,
+                            z = player.PlayerPawn?.Value?.AbsOrigin!.Z ?? 0
+                        },
+                        angle = new
+                        {
+                            pitch = player.PlayerPawn?.Value?.EyeAngles.X ?? 0,
+                            yaw = player.PlayerPawn?.Value?.EyeAngles.Y ?? 0,
+                        }
+                    }).ToList()
+                }
+            });
+        });
+    }
+
+    public async void SendEvent(object eventData)
+    {
+        using (var client = new HttpClient())
+        {
+            var json = JsonSerializer.Serialize(eventData);
+            var content = new StringContent(json, Encoding.UTF8, new MediaTypeHeaderValue("application/json"));
+            var response = await client.PostAsync("http://localhost:3000/api/gamestate", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Logger.LogError("Failed to send event. Status code: {StatusCode}", response.StatusCode);
+            }
+        }
+    }
+
+    [GameEventHandler]
+    public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
+    {
+        if (@event.Userid != null && @event.Userid.IsValid)
+        {
+            Logger.LogInformation("Player {Name} has connected!", @event.Userid.PlayerName);
+        }
+
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler]
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+    {
+        if (@event.Userid != null && @event.Userid.IsValid)
+        {
+            Logger.LogInformation("Player {Name} has disconnected!", @event.Userid.PlayerName);
+        }
+
+        return HookResult.Continue;
     }
 }
--- projects/terrorlink-server/index.ts
+++ projects/terrorlink-server/index.ts
@@ -5,22 +5,8 @@ import { existsSync, mkdirSync, rmdirSync, writeFileSync } from "node:fs";
 console.log("Hello via Bun!");
 const port = Number.parseInt(env.WEBSERVER_PORT as string);
 
-const messagesDirectory = "./messages";
-
-if (existsSync(messagesDirectory)) {
-	rmdirSync(messagesDirectory, { recursive: true });
-}
-
-mkdirSync(messagesDirectory);
-let messageCount = -1;
-
 new Elysia()
-	.post("/api/gsi", (context) => {
-		console.log(context.body);
-		messageCount++;
-		writeFileSync(
-			`${messagesDirectory}/message-${messageCount}.json`,
-			JSON.stringify(context.body, null, 2),
-		);
+	.post("/api/gamestate", (context) => {
+		console.dir(context.body, { depth: null });
 	})
 	.listen(port, () => console.log(`Server is running on ${port}`));
