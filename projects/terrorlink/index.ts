4f263a96e06b57d86a84750b7a92fffc7c1c56ee
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-02-24 19:07:07 +0100

    connect to event socket

--- projects/terrorlink/index.ts
+++ projects/terrorlink/index.ts
@@ -24,15 +24,11 @@ const BASE_URL =
 const PORT = await getPort();
 
 console.log(`http://localhost:${PORT}`);
-const rpc = new Client({
-	clientId: "1342884301196496998",
-	// transport: { type: "websocket" },
-});
-await rpc.connect();
-console.log(retrieveToken("discord"));
-await rpc.authenticate(decodeJWT(retrieveToken("discord")).accessToken);
-
-new Window(`http://localhost:${PORT}`);
+const window = new Window(`http://localhost:${PORT}`);
+let discordRPC: Client | null;
+let websocket: WebSocket | null;
+let messageCount = 0; // Received message count
+let bytesCount = 0; // Received data count in bytes
 
 function decodeJWT(token: string) {
 	try {
@@ -49,12 +45,15 @@ function storeToken(token: string) {
 	if (!decodedToken.type) return;
 	data[decodedToken.type] = token;
 	writeJSONSync("./terrorlink.data", data, { spaces: 2 });
+	connect();
+	window.reload();
 }
 
 function removeToken(type: string) {
 	const data = readJsonSync("./terrorlink.data", { throws: false }) ?? {};
 	delete data[type];
 	writeJSONSync("./terrorlink.data", data, { spaces: 2 });
+	disconnect();
 }
 
 function retrieveToken(type: string) {
@@ -62,12 +61,71 @@ function retrieveToken(type: string) {
 	return data[type] ?? false;
 }
 
-const websocket = new WebSocket(`${env.WEBSOCKET_URL}/api/socket`, {
-	headers: {
-		"x-discord-token": retrieveToken("discord"),
-		"x-steam-token": retrieveToken("steam"),
-	},
-} as unknown as string[]);
+async function disconnect() {
+	console.log("Disconnecting...");
+	websocket?.close();
+	websocket = null;
+	discordRPC?.destroy();
+	discordRPC = null;
+}
+
+async function connect() {
+	disconnect();
+	const discordToken = retrieveToken("discord");
+	const steamToken = retrieveToken("steam");
+	if (!discordToken || !steamToken) {
+		console.log("Not connecting as not all tokens are stored");
+		return;
+	}
+	console.log("Connecting to websocket...");
+
+	discordRPC = new Client({
+		clientId: "1342884301196496998",
+		// transport: { type: "websocket" },
+	});
+
+	discordRPC.on("ready", () => {
+		console.log("RPC ready...");
+	});
+
+	discordRPC.on("disconnected", () => {
+		console.log("RPC disconnected...");
+		reconnect();
+	});
+
+	await discordRPC.connect();
+	await discordRPC.authenticate(decodeJWT(discordToken).accessToken);
+
+	websocket = new WebSocket(`${env.WEBSOCKET_URL}/api/socket`, {
+		headers: {
+			"x-discord-token": discordToken,
+			"x-steam-token": steamToken,
+		},
+	} as unknown as string[]);
+
+	websocket.addEventListener("open", () => {
+		console.log("Websocket connected...");
+	});
+
+	websocket.addEventListener("close", () => {
+		console.log("Websocket disconnected...");
+		reconnect();
+	});
+
+	websocket.addEventListener("message", (event) => {
+		messageCount++;
+		bytesCount += event.data.length;
+	});
+
+	console.log("Connected");
+}
+
+async function reconnect() {
+	await disconnect();
+	await connect();
+}
+
+connect();
 
 serve({
 	development: true,
@@ -122,6 +180,10 @@ serve({
 			return Response.json({
 				discord: discordToken ? decodeJWT(discordToken) : false,
 				steam: steamToken ? decodeJWT(steamToken) : false,
+				discordRPC: !!discordRPC,
+				websocket: !!websocket,
+				messageCount,
+				bytesCount,
 			});
 		},
 	},
