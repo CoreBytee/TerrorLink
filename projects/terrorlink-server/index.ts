14aa03be11e1f30eda3d0f98f5dd29b9520de116
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-03-28 22:38:24 +0100

    Update index.ts

--- projects/terrorlink-server/index.ts
+++ projects/terrorlink-server/index.ts
@@ -1,12 +1,3 @@
-import { env } from "bun";
-import Elysia from "elysia";
-import SteamAuthentication from "./classes/SteamAuthentication";
-import jwt from "jwt-simple";
-import type { ElysiaWS } from "elysia/ws";
-import type { GameStateBody } from "./types/GameStateBody";
 import TerrorLinkServer from "./classes/TerrorLinkServer";
 
-console.log("Hello via Bun!");
-const port = Number.parseInt(env.NETWORK_PORT as string);
-
-new TerrorLinkServer(port);
+new TerrorLinkServer();
