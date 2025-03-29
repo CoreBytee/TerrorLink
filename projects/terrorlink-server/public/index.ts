2697381c8c63c5487b84952d4431ac28c39578e3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-03-29 19:42:49 +0100

    Update index.ts

--- projects/terrorlink-server/public/index.ts
+++ projects/terrorlink-server/public/index.ts
@@ -401,9 +401,11 @@ class TerrorLink {
 				this.speaker.setPosition(me?.position, me?.angle);
 
 				positions.forEach((player) => {
+					console.log(player.me, player.peer_id);
 					if (!this.speaker.channelExists(player.peer_id)) return;
 					if (player.me) return;
 					if (!player.peer_id) return;
+					console.log("passed");
 					this.speaker.setChannelPosition(
 						player.peer_id,
 						player.position,
