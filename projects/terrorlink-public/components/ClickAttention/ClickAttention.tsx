bf5f5829f527b87970168844b4b90d5dce294ecd
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-04-12 00:21:02 +0200

    debounce ClickAttention

--- projects/terrorlink-public/components/ClickAttention/ClickAttention.tsx
+++ projects/terrorlink-public/components/ClickAttention/ClickAttention.tsx
@@ -1,12 +1,24 @@
 import { HandIndex } from "react-bootstrap-icons";
 import "./ClickAttention.css";
+import { useEffect, useState } from "react";
 
 export default function ClickAttention({
 	active = false,
 }: {
 	active: boolean;
 }) {
-	if (!active) return null;
+	const [isVisible, setIsVisible] = useState(false);
+
+	useEffect(() => {
+		if (active) {
+			const timer = setTimeout(() => setIsVisible(true), 100);
+			return () => clearTimeout(timer);
+		}
+		setIsVisible(false);
+	}, [active]);
+
+	if (!isVisible) return null;
+
 	return (
 		<div className="ClickAttention">
 			<HandIndex size={window.innerWidth / 3} />
