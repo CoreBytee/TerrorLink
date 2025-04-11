f55a2ad177035667c623bdfcf3298070f7abccd1
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-04-11 14:20:40 +0200

    work on login page

--- projects/terrorlink-server/public/screens/Login/Login.tsx
+++ projects/terrorlink-server/public/screens/Login/Login.tsx
@@ -4,6 +4,11 @@ export function Login() {
 	return (
 		<div className={"Login"}>
 			<h1>Login</h1>
+			<p>
+				You must be logged in using Steam to use TerrorLink. You can login by
+				pressing the button below.
+			</p>
+			<button type="button">Login using Steam</button>
 		</div>
 	);
 }
