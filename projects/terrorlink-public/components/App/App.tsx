7644df05c6331ff420bf36ca836584cd021469b7
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-04-12 14:14:05 +0200

    connection logic

--- projects/terrorlink-public/components/App/App.tsx
+++ projects/terrorlink-public/components/App/App.tsx
@@ -10,6 +10,8 @@ import useSpeaker from "../../hooks/useSpeaker";
 import useMicrophone from "../../hooks/useMicrophone";
 import ClickAttention from "../ClickAttention/ClickAttention";
 import useEvents from "../../hooks/useEvents";
+import { MessageType, type MessageUpdatePositionsPayload } from "networking";
+import type { DataConnection, MediaConnection } from "peerjs";
 
 type AuthenticationStatus = {
 	authenticated: boolean;
@@ -35,6 +37,7 @@ export default function App() {
 	// Connect to events socket
 	useEffect(() => {
 		function onOpen() {
+			console.info("My peer ID is", peer.id);
 			events.connect(peer.id);
 		}
 
@@ -48,6 +51,50 @@ export default function App() {
 		return () => {
 			peer.off("open", onOpen);
 			events.off("connect", onConnect);
+		};
+	});
+
+	// Handle peer connections
+	useEffect(() => {
+		function onGamestate(payload: MessageUpdatePositionsPayload) {
+			const players = payload.players;
+
+			for (const player of players) {
+				if (player.me) continue;
+				if (!player.peer_id) continue;
+				// @ts-ignore
+				if (peer.connections[player.peer_id]) continue;
+				console.info("Calling peer", player.peer_id);
+				peer.call(player.peer_id, microphone.stream);
+				console.log(player);
+			}
+
+			for (const connectionEntry of Object.entries(peer.connections)) {
+				const [peerId, connections] = connectionEntry;
+				const player = players.find((p) => p.peer_id === peerId);
+				if (player) continue;
+				console.info("Closing connection to peer", peerId);
+				connections.forEach((connection: MediaConnection | DataConnection) => {
+					connection.close();
+				});
+				speaker.removeChannel(peerId);
+			}
+		}
+
+		function onCall(call: MediaConnection) {
+			console.info("Incoming call from", call.peer);
+			call.answer();
+
+			call.once("stream", (stream) => {
+				speaker.createChannel(call.peer, stream);
+			});
+		}
+
+		events.on(MessageType.UpdatePositions, onGamestate);
+		peer.addListener("call", onCall);
+		return () => {
+			events.off(MessageType.UpdatePositions, onGamestate);
+			peer.removeListener("call", onCall);
 		};
 	});
 
